422f7079785224616831daa686f9c0a126671a7a
leminhduy1912 <minhduyle081003@gmail.com>
2023-03-30 15:57:50 +0700

    formAll

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import './App.css';
 import Login from './Component/Login/Login';
 import Sidebar from './Component/Admin/Sidebar/Sidebar';
 import Dashboard from './Component/Admin/Dashboard/Dashboard';
@@ -17,8 +18,11 @@ import HomePage from './Component/HomePage/HomePage';
 import ManageAccount from './Component/Admin/ManagerAccount/ManageAccount';
 import AccountUpdate from './Component/Admin/ManagerAccount/AccountUpdate/AccountUpdate';
 import PlanInspect from './Component/Admin/PlanInspect/PlanInspect';
-import './App.css';
 import Stores from './Component/Admin/Stores/Stores';
+import UpdateStore from './Component/Admin/Stores/UpdateStore/UpdateStore';
+import InforSelf from './Component/Admin/InforSelf/InforSelf';
+import AddStore from './Component/Admin/Stores/AddStore/AddStore';
+import AddAccount from './Component/Admin/ManagerAccount/AddAccount/AddAccount';
 
 
 function App() {
@@ -33,10 +37,17 @@ function App() {
       <Route  path='/loading' element={<Loading/>}/>
       <Route path='/admin' element={<Sidebar data={SidebarData} role={"Admin"}  />}>
         <Route path='dashBoard' element={<Dashboard/>}/>
+        {/* Account */}
         <Route path='manageAccount' element={<ManageAccount/>}/>
         <Route path='accountUpdate' element={<AccountUpdate/>}/>
+        <Route path='addAccount' element={<AddAccount/>}/>
+
+        
         <Route path='planInspect' element={<PlanInspect/>}/>
         <Route path='stores' element={<Stores/>}/>
+        <Route path='storesUpdate' element={<UpdateStore/>}/>
+        <Route path='addStore' element={<AddStore/>}/>
+        <Route path='inforSelf' element={<InforSelf/>}/>
       </Route>
       <Route path='/moderrator' element={<Sidebar data={SidebarData}  role={"Moderator"} />}>
         <Route path='dashBoard' element={<Dashboard/>}/>
